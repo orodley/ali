8a7b8f24e34a6df551fd32df4ced1f32ffa86415
Owen Rodley <Strigoides@gmail.com>
2013-06-03 18:17:02 +1200

    Invert NULL check

--- read.c
+++ read.c
@@ -32,68 +32,67 @@ struct LispObj *read_from_yybuf(YY_BUFFER_STATE yy_buf)
 	yy_switch_to_buffer(yy_buf);
 	struct Token token = get_token();
 
-	if (token.str != NULL) { /* a NULL pointer = EOF */
-		switch (token.type) {
-			case T_INTEGER: {
-				int x;
-				sscanf(token.str, "%d", &x);
-
-				free(token.str);
-				return make_int(x);
-			}
-			case T_STRING: {
-				int len = strlen(token.str);
-
-				/* Take a substring of token.str, skipping the first
-				 * and last chars */
-				char *str = malloc(sizeof(char) * (len - 2));
-				strncpy(str, token.str + 1, len - 2);
-				*(str + (len - 2)) = '\0';
-
-				free(token.str);
-				return make_string(str);
-			}
-			case T_SYMBOL: {
-				int len = strlen(token.str);
-
-				char *str = malloc(sizeof(char) * len);
-				strncpy(str, token.str, len);
-				*(str + len) = '\0';
-
-				free(token.str);
-				return make_symbol(str);
-			}
-			case T_OPEN_PAREN: {
-				struct Token car = get_token();
-
-				if (car.str == NULL) /* Unmatched open parenthesis */
-					return NULL;     /* TODO: Error handling/reporting */
-
-				if (car.type == T_CLOSE_PAREN)
-					return get_nil();
-
-				struct LispObj *first_obj = read_from_string(car.str);
-				yy_switch_to_buffer(yy_buf); /* Switch back to the buffer */
-
-				struct Cons *curr_cons = cons(first_obj, get_nil());
-				struct LispObj *list = make_cons(curr_cons);
-
-				while ((car = get_token()).type != T_CLOSE_PAREN) {
-					if (car.str == NULL)
-						return NULL;
-
-					first_obj = read_from_string(car.str);
-					yy_switch_to_buffer(yy_buf);
-
-					curr_cons->cdr = make_cons(cons(first_obj, get_nil()));
-					curr_cons = curr_cons->cdr->value.l_cons;
-				}
-
-				return list;
-			}
-		}
-	} else {
+	if (token.str == NULL) /* a NULL pointer = EOF */
 		return NULL;
+
+	switch (token.type) {
+		case T_INTEGER: {
+			int x;
+			sscanf(token.str, "%d", &x);
+
+			free(token.str);
+			return make_int(x);
+		}
+		case T_STRING: {
+			int len = strlen(token.str);
+
+			/* Take a substring of token.str, skipping the first
+			 * and last chars */
+			char *str = malloc(sizeof(char) * (len - 2));
+			strncpy(str, token.str + 1, len - 2);
+			*(str + (len - 2)) = '\0';
+
+			free(token.str);
+			return make_string(str);
+		}
+		case T_SYMBOL: {
+			int len = strlen(token.str);
+
+			char *str = malloc(sizeof(char) * len);
+			strncpy(str, token.str, len);
+			*(str + len) = '\0';
+
+			free(token.str);
+			return make_symbol(str);
+		}
+		case T_OPEN_PAREN: {
+			struct Token car = get_token();
+
+			if (car.str == NULL) /* Unmatched open parenthesis */
+				return NULL;     /* TODO: Error handling/reporting */
+
+			if (car.type == T_CLOSE_PAREN)
+				return get_nil();
+
+			struct LispObj *first_obj = read_from_string(car.str);
+			yy_switch_to_buffer(yy_buf); /* Switch back to the buffer */
+
+			struct Cons *curr_cons = cons(first_obj, get_nil());
+			struct LispObj *list = make_cons(curr_cons);
+
+			while ((car = get_token()).type != T_CLOSE_PAREN) {
+				if (car.str == NULL)
+					return NULL;
+
+				first_obj = read_from_string(car.str);
+				yy_switch_to_buffer(yy_buf);
+
+				curr_cons->cdr = make_cons(cons(first_obj, get_nil()));
+				curr_cons = curr_cons->cdr->value.l_cons;
+			}
+
+			return list;
+		}
 	}
 }
 
